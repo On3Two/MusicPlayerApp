883e70afeda16c0afd9466ac11963a049a0c8888
Classified <Tdominic91@gmail.com>
2023-06-18 00:03:31 -0400

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   assets/2AM.mp3
    	new file:   assets/2amcover.jpg
    	new file:   assets/AllMyLife.mp3
    	renamed:    assets/sheff.jpg -> assets/amfcover.jpg
    	modified:   index.html
    	modified:   index.js
    	modified:   style.css

--- index.js
+++ index.js
@@ -17,30 +17,81 @@ const songs = [
     {
         path: 'assets/AllMyLife.mp3',
         displayName: "All my life",
-        cover: 'assets/amfcover',
+        cover: 'assets/amfcover.jpg',
         artist: 'Sheff G (Feat. Sleepy Hallow)'
     },
     {
         path: 'assets/2AM.mp3',
-        displayName: "2A.M.",
-        cover: 'assets/2amcover',
+        displayName: "2 A.M.",
+        cover: 'assets/2amcover.jpg',
         artist: 'King Von'
     },
-    {
-        path: 'assets/762god.mp3',
-        displayName: "7.62 God",
-        cover: 'assets/2amcover',
-        artist: 'Pooh Shiesty'
-    }
 ]
 
 let musicIndex = 0;
 let isPlaying = false;
 
-function togglePlay(){
-    if(isPlaying){
+function togglePlay() {
+    if (isPlaying) {
         pauseMusic();
-    }else{
+    } else {
         playMusic();
     }
 }
+
+function playMusic() {
+    isPlaying = true;
+    // Change play button icon
+    playBtn.classList.replace('fa-play', 'fa-pause');
+    // Set button hover title
+    playBtn.setAttribute('title', 'Pause');
+    music.play();
+}
+
+function pauseMusic() {
+    isPlaying = false;
+    // Change pause button icon
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    // Set button hover title
+    playBtn.setAttribute('title', 'Play');
+    music.pause();
+}
+
+function loadMusic(song) {
+    music.src = song.path;
+    title.textContent = song.displayName;
+    artist.textContent = song.artist;
+    image.src = song.cover;
+    background.src = song.cover;
+}
+
+function changeMusic(direction) {
+    musicIndex = (musicIndex + direction + songs.length) % songs.length;
+    loadMusic(songs[musicIndex]);
+    playMusic();
+}
+
+function updateProgressBar() {
+    const { duration, currentTime } = music;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+
+    const formatTime = (time) => String(Math.floor(time)).padStart(2, '0');
+    durationEl.textContent = `${formatTime(duration / 60)}:${formatTime(duration % 60)}`;
+    currentTimeEl.textContent = `${formatTime(currentTime / 60)}:${formatTime(currentTime % 60)}`;
+}
+
+function setProgressBar(e) {
+    const width = playerProgress.clientWidth;
+    const clickX = e.offsetX;
+    music.currentTime = (clickX / width) * music.duration;
+}
+
+playBtn.addEventListener('click', togglePlay);
+prevBtn.addEventListener('click', () => changeMusic(-1));
+nextBtn.addEventListener('click', () => changeMusic(1));
+music.addEventListener('ended', () => changeMusic(1));
+music.addEventListener('timeupdate', updateProgressBar);
+playerProgress.addEventListener('click', setProgressBar);
+
+loadMusic(songs[musicIndex]);
